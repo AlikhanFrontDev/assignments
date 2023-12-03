2a24be58437a8c769d6d9f60d4a0193f7afa2ea4
AlikhanFrontDev <alixanhellion@gmail.com>
2023-12-03 10:40:20 +0100

    dfdfds

--- src/pages/task1.jsx
+++ src/pages/task1.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-
+import file1 from "../pages/files/hw1.pdf";
 
 export default function Task1() {
   return (
     <Container>
-        <h1>Unit 1 Homework</h1>
+      <h1>Unit 1 Homework</h1>
+      <div className="button">
+        <a href={file1} target="_blank">
+          Download PDF file
+        </a>
+      </div>
     </Container>
-  )
+  );
 }
 const Container = styled.div`
-    background-color: #00000080;
-    height: 50vh;
-    h1{
-        text-align: center;
-        padding: 20px 0;
-    }
-`
+  background-color: #00000080;
+  height: 50vh;
+  .button{
+    text-align: center;
+    margin-top: 100px;
+
+  }
+  .button a{
+    padding: 50px 100px;
+    border-radius: 12px;
+    background-color: #fff;
+    text-decoration: none;
+    color: #000;
+    font-size: 20px;
+  }
+  h1 {
+    text-align: center;
+    padding: 20px 0;
+  }
+`;
