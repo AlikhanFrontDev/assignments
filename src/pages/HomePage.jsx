8c2e481fb80d7391e1f1d72498f1edcc30e9c697
AlikhanFrontDev <alixanhellion@gmail.com>
2023-12-03 10:53:21 +0100

    sdsd

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Mainpage from './mainpage'
 import Task1 from './task1'
+import Task2 from './task2'
 
 export default function HomePage() {
   return (
     <div>
         <Mainpage/>
         <Task1/>
+        <Task2/>
     </div>
   )
 }
--- src/pages/task1.jsx
+++ src/pages/task1.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import file1 from "../pages/files/hw1.pdf";
+import A from "../pages/files/A.bmp";
+import B from "../pages/files/B.bmp";
+import C from "../pages/files/C.bmp";
+import E from "../pages/files/E.jpg";
 
 export default function Task1() {
   return (
     <Container>
       <h1>Unit 1 Homework</h1>
+      <div className="images">
+        <img src={A} alt="" />
+        <img src={B} alt="" />
+        <img src={C} alt="" />
+        <img src={E} alt="" />
+      </div>
       <div className="button">
         <a href={file1} target="_blank">
           Download PDF file
@@ -16,14 +26,24 @@ export default function Task1() {
 }
 const Container = styled.div`
   background-color: #00000080;
-  height: 50vh;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  .images{
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+  img{
+    width: 300px;
+  }
   .button{
     text-align: center;
-    margin-top: 100px;
-
+    /* padding: 50px 100px; */
   }
   .button a{
-    padding: 50px 100px;
+    padding: 25px 50px;
     border-radius: 12px;
     background-color: #fff;
     text-decoration: none;
