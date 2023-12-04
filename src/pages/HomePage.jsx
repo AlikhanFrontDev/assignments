74e8a21f1bda2d41a8776067d79c09b1f3434220
AlikhanFrontDev <alixanhellion@gmail.com>
2023-12-04 09:55:26 +0100

    qwqwq

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Mainpage from './mainpage'
 import Task1 from './task1'
 import Task2 from './task2'
+import Output from './output'
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
         <Mainpage/>
         <Task1/>
         <Task2/>
+        <Output/>
     </div>
   )
 }
---
+++ src/pages/output.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import img1 from "../pages/files/download1.png"
+import img2 from "../pages/files/download2.png"
+import img3 from "../pages/files/images1.png"
+import styled from 'styled-components'
+
+export default function Output() {
+  return (
+    <Container>
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    margin: 50px 0;
+    img{
+        width: 420px;
+        object-fit: cover;
+
+    }
+`
